8fc92340afc35e0730900ed36608f08780b4a0d4
Will Velida <willvelida@hotmail.co.uk>
2024-06-11 05:24:46 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  var member = {
+    name: "John Doe",
+    age: 25,
+    profession: "developer",
+    skills: ["JavaScript", "TypeScript", "React"]
+  };
+
+  return member;
+}
